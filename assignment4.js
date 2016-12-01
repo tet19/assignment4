38dff374e625b370f6e67f8905b82ad65c425599
tet19 <tet19@pitt.edu>
2016-12-01 00:26:13 -0500

    finished assignment

--- assignment4.js
+++ assignment4.js
@@ -15,3 +15,46 @@
   // Magic!
   console.log('Keepin\'n it clean with an external script!');
 })();
+
+
+
+//check to see new entry whenever key is released
+$('.flexsearch-input').keyup(function(event){
+  var api = 'http://www.mattbowytz.com/simple_api.json?data=all';
+  var output = '';
+  var current_input = $('.flexsearch-input').val().toLowerCase();
+  if(current_input.length >= 1){
+    $.ajax({
+      dataType: 'json',
+      url: api,
+      success: function(data){
+        $.each(data.data, function(key, subject){
+          $.each(subject, function(key2, value){
+            valueNoCase = value.toLowerCase();
+            if(valueNoCase.indexOf(current_input) != -1){
+              output += "<button class=autofill-box onclick=autofill(this) value='"+ value + "'>"+ value +"</button>";
+            }
+          });
+        });
+        $('.autocomplete').html(output);
+      },
+      error: function(){
+        console.log("Error getting api");
+      }
+    });
+  }
+  else{
+    $('.autocomplete').html(output);
+  }
+});
+
+function autofill(button){
+  $('.flexsearch-input').val(button.value);
+  window.open('http://www.google.com/search?q=' + button.value,'_blank');
+}
+
+$('.flexsearch').submit(function(event){
+  event.preventDefault();
+  var input = $('.flexsearch-input').val();
+  window.open('http://www.google.com/search?q=' + input,'_blank');
+});
